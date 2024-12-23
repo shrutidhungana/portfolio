5a39da525274d00495aaca28273ba68d2d55a680
shrutidhungana <shrutidhungana123@gmail.com>
2024-12-23 21:32:26 +0545

    implemented message send: 1)Added env 2)used emailjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <ToastContainer />
       <Component {...pageProps} />
     </>
   );
