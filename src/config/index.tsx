77ce37ed0fe534753376b110a27235a93cc1b17b
shrutidhungana <shrutidhungana123@gmail.com>
2024-12-30 21:34:08 +0545

    Projects details completed: 1)Added tags and all

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -24,9 +24,16 @@ import {
   FaShoppingCart,
   FaGamepad,
   FaClone,
+  FaUtensils,
+  FaCalculator,
 } from "react-icons/fa";
-import { SiCreatereactapp, SiShadcnui, SiStyledcomponents, SiVite } from "react-icons/si";
-import { FaLocationDot } from "react-icons/fa6";
+import {
+  SiCreatereactapp,
+  SiShadcnui,
+  SiStyledcomponents,
+  SiVite,
+} from "react-icons/si";
+import { FaAffiliatetheme, FaLocationDot } from "react-icons/fa6";
 import { IoMdDownload } from "react-icons/io";
 import CSS from "@/components/SVG/Skills/css";
 import Firebase from "@/components/SVG/Skills/firebase";
@@ -43,6 +50,8 @@ import ReactSVG from "@/components/SVG/Skills/react";
 import Tailwind from "@/components/SVG/Skills/tailwind";
 import Typescript from "@/components/SVG/Skills/ts";
 import Express from "@/components/SVG/Skills/express";
+import { FcDecision } from "react-icons/fc";
+import { IoShareSocial } from "react-icons/io5";
 
 export const links: NavLink[] = [
   {
@@ -355,31 +364,35 @@ export const projectData: ProjectsData[] = [
     website: "https://e-commerce-hazel-iota-68.vercel.app/",
     details: [
       {
-        icon: <FaShoppingCart />,
+        icon: <FaShoppingCart className="text-2xl" />,
         text: "E-commerce website that allows users to browse and purchase products online.",
       },
       {
-        icon: <MongoDB className="text-2xl" />,
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <MongoDB className="text-xl" />,
         text: "MongoDB",
       },
       {
-        icon: <Express className="text-2xl" />,
+        icon: <Express className="text-xl" />,
         text: "Express",
       },
       {
-        icon: <Next className="text-2xl" />,
+        icon: <Next className="text-xl" />,
         text: "Next.js",
       },
       {
-        icon: <Node className="text-2xl" />,
+        icon: <Node className="text-xl" />,
         text: "Node.js",
       },
       {
-        icon: <Tailwind className="text-2xl" />,
+        icon: <Tailwind className="text-xl" />,
         text: "Tailwind CSS",
       },
       {
-        icon: <Typescript className="text-2xl" />,
+        icon: <Typescript className="text-xl" />,
         text: "TypeScript",
       },
     ],
@@ -396,23 +409,27 @@ export const projectData: ProjectsData[] = [
         text: "Clone of Leetcode, a popular platform for practicing coding problems and preparing for interviews.",
       },
       {
-        icon: <Next className="text-2xl" />,
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <Next className="text-xl" />,
         text: "Next.js",
       },
       {
-        icon: <Tailwind className="text-2xl" />,
+        icon: <Tailwind className="text-xl" />,
         text: "Tailwind CSS",
       },
       {
-        icon: <Typescript className="text-2xl" />,
+        icon: <Typescript className="text-xl" />,
         text: "TypeScript",
       },
       {
-        icon: <Firebase className="text-2xl" />,
+        icon: <Firebase className="text-xl" />,
         text: "Firebase",
       },
       {
-        icon: <SiShadcnui className="text-2xl" />,
+        icon: <SiShadcnui className="text-xl" />,
         text: "ShadCN",
       },
     ],
@@ -429,19 +446,23 @@ export const projectData: ProjectsData[] = [
         text: "A simple Tic Tac Toe app built with React.",
       },
       {
-        icon: <ReactSVG className="text-2xl" />,
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
         text: "React",
       },
       {
-        icon: <SiVite className="text-2xl" />,
+        icon: <SiVite className="text-xl" />,
         text: "Vite",
       },
       {
-        icon: <Typescript className="text-2xl" />,
+        icon: <Typescript className="text-xl" />,
         text: "TypeScript",
       },
       {
-        icon: <CSS className="text-2xl" />,
+        icon: <CSS className="text-xl" />,
         text: "CSS",
       },
     ],
@@ -459,19 +480,23 @@ export const projectData: ProjectsData[] = [
         text: "A shopping cart app built in React using data from Fakestore API to add items to the cart.",
       },
       {
-        icon: <ReactSVG className="text-2xl" />,
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
         text: "React",
       },
       {
-        icon: <Javascript className="text-2xl" />,
+        icon: <Javascript className="text-xl" />,
         text: "JavaScript",
       },
       {
-        icon: <SiCreatereactapp className="text-2xl" />,
+        icon: <SiCreatereactapp className="text-xl" />,
         text: "Create React App",
       },
       {
-        icon: <CSS className="text-2xl" />,
+        icon: <CSS className="text-xl" />,
         text: "CSS",
       },
     ],
@@ -488,15 +513,19 @@ export const projectData: ProjectsData[] = [
         text: "A movie search app that allows users to search and explore movies.",
       },
       {
-        icon: <ReactSVG className="text-2xl" />,
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
         text: "React",
       },
       {
-        icon: <Javascript className="text-2xl" />,
+        icon: <Javascript className="text-xl" />,
         text: "JavaScript",
       },
       {
-        icon: <SiStyledcomponents className="text-2xl" />,
+        icon: <SiStyledcomponents className="text-xl" />,
         text: "Styled Components",
       },
     ],
@@ -507,6 +536,28 @@ export const projectData: ProjectsData[] = [
     image: "/projects/khana-sansar.png",
     github: "https://github.com/shrutidhungana/khanasansar",
     website: "https://khanasansar.vercel.app/",
+    details: [
+      {
+        icon: <FaUtensils className="text-2xl" />, // A more relevant icon for food/recipe
+        text: "A food recipe app built in React that uses the MealDB API to help users search and find recipes for their preferred dishes.",
+      },
+      {
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
+        text: "React",
+      },
+      {
+        icon: <Javascript className="text-xl" />,
+        text: "JavaScript",
+      },
+      {
+        icon: <SiVite className="text-xl" />,
+        text: "Vite",
+      },
+    ],
   },
   {
     id: 7,
@@ -514,6 +565,28 @@ export const projectData: ProjectsData[] = [
     image: "/projects/calculator.png",
     github: "https://github.com/shrutidhungana/calculator",
     website: "https://shrutidhungana.github.io/calculator/",
+    details: [
+      {
+        icon: <FaCalculator className="text-2xl" />, // A more relevant icon for food/recipe
+        text: "A simple calculator app to perform arithmetic operations.",
+      },
+      {
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
+        text: "React",
+      },
+      {
+        icon: <Javascript className="text-xl" />,
+        text: "JavaScript",
+      },
+      {
+        icon: <SiVite className="text-xl" />,
+        text: "Vite",
+      },
+    ],
   },
   {
     id: 8,
@@ -521,6 +594,32 @@ export const projectData: ProjectsData[] = [
     image: "/projects/indecision-app.png",
     github: "https://github.com/shrutidhungana/indecision-app",
     website: "https://shrutidhungana.github.io/indecision-app/",
+    details: [
+      {
+        icon: <FcDecision className="text-2xl" />,
+        text: "An app where computer decides what to do for you.",
+      },
+      {
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
+        text: "React",
+      },
+      {
+        icon: <Javascript className="text-xl" />,
+        text: "JavaScript",
+      },
+      {
+        icon: <SiCreatereactapp className="text-xl" />,
+        text: "Create React App",
+      },
+      {
+        icon: <CSS className="text-xl" />,
+        text: "CSS",
+      },
+    ],
   },
   {
     id: 9,
@@ -528,6 +627,40 @@ export const projectData: ProjectsData[] = [
     image: "/projects/sociopedia.png",
     github: "https://github.com/shrutidhungana/sociopedia",
     website: "https://social-media-gamma-six.vercel.app/",
+    details: [
+      {
+        icon: <IoShareSocial className="text-2xl" />,
+        text: "A full-stack social media app to post your thoughts, make friends etc..",
+      },
+      {
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <MongoDB className="text-xl" />,
+        text: "MongoDB",
+      },
+      {
+        icon: <Express className="text-xl" />,
+        text: "Express",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
+        text: "React",
+      },
+      {
+        icon: <Node className="text-xl" />,
+        text: "Node.js",
+      },
+      {
+        icon: <Material className="text-xl" />,
+        text: "Material UI",
+      },
+      {
+        icon: <Javascript className="text-xl" />,
+        text: "JavaScript",
+      },
+    ],
   },
   {
     id: 10,
@@ -535,5 +668,31 @@ export const projectData: ProjectsData[] = [
     image: "/projects/react-theme.png",
     github: "https://github.com/shrutidhungana/react-theme",
     website: "https://shrutidhungana.github.io/react-theme/",
+    details: [
+      {
+        icon: <FaAffiliatetheme className="text-2xl" />,
+        text: "An app to toggle between light and dark theme.",
+      },
+      {
+        icon: <FaCode className="text-2xl" />,
+        text: "It is built in following tools and languages:",
+      },
+      {
+        icon: <ReactSVG className="text-xl" />,
+        text: "React",
+      },
+      {
+        icon: <Javascript className="text-xl" />,
+        text: "JavaScript",
+      },
+      {
+        icon: <SiCreatereactapp className="text-xl" />,
+        text: "Create React App",
+      },
+      {
+        icon: <CSS className="text-xl" />,
+        text: "CSS",
+      },
+    ],
   },
 ];
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PortfolioLayout from "@/components/layout";
 import { projectData } from "@/config";
 import ProjectCard from "@/components/Card";
+import ProjectDetailsDialog from "@/components/Dialog";
+
 
 const Projects: React.FC = () => {
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleOpenDialog = (project: any) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseDialog = () => {
+    setSelectedProject(null);
+  };
+
   return (
     <PortfolioLayout>
       <div className="bg-gradient-to-r from-[#FDEBD0] to-[#F9D7E3] min-h-screen py-12 px-6">
@@ -18,10 +30,19 @@ const Projects: React.FC = () => {
               image={project.image}
               github={project.github}
               website={project.website}
+              onViewDetails={() => handleOpenDialog(project)} // Pass the project data here
             />
           ))}
         </div>
       </div>
+
+      {/* Conditionally render the dialog if a project is selected */}
+      {selectedProject && (
+        <ProjectDetailsDialog
+          project={selectedProject}
+          onClose={handleCloseDialog}
+        />
+      )}
     </PortfolioLayout>
   );
 };
